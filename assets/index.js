e44dc5c4f418c51d13985bea55b95e249c4b1031
donghohnation <donghoh.han@gmail.com>
2022-09-06 15:39:21 -0700

    files added

---
+++ assets/index.js
@@ -0,0 +1,288 @@
+const quote = document.querySelector("#quote");
+const author = document.querySelector("#author");
+const btn = document.querySelector("#btn");
+
+
+
+const advicee = document.querySelector("#advice");
+const btnAdvice = document.querySelector("#btnAdvice");
+
+
+
+
+window.onload = getQuote(); getAdvice();
+
+btn.addEventListener("click", getQuote);
+
+function getQuote() {
+	fetch("http://quotable.io/random")
+		.then(res => res.json())
+		.then(data => {
+			quote.innerHTML = `"${data.content}"`;
+			author.innerHTML = `-${data.author}`;
+		})
+}
+
+
+
+btnAdvice.addEventListener("click", getAdvice);
+
+function getAdvice() {
+	fetch("https://api.adviceslip.com/advice").
+		then(response => {
+			return response.json();
+		}).then(adviceData => {
+			const Adviceobj = adviceData.slip;
+			advicee.innerHTML = `"${Adviceobj.advice}"`;
+		}).catch(error => {
+			console.log(error);
+		});
+
+}
+
+// function getAdvice() {
+// 	fetch("https://api.adviceslip.com/advice")
+// 		.then(res => res.json())
+// 		.then(data => {
+// 			advicee.innerHTML = `${data.slip.advice}`;
+// 		})
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const enter = document.getElementById("cityInp")
+enter.addEventListener("keypress", (event) => {
+	if (event.keyCode === 13) {
+		getInfo()
+	}
+})
+
+// enter city and click search, header2 will show searched city
+
+const searchbtn = document.getElementById("searchbtn")
+searchbtn.addEventListener("click", getInfo);
+
+function getInfo() {
+	const searchedCity = document.getElementById("cityInp");
+	const cityName = document.getElementById("cityName");
+	cityName.innerHTML = `${searchedCity.value}`;
+
+	// ______________________________________________
+
+	fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchedCity.value}&units=imperial&appid=f419cd27b5d7777610c83ec9680dce19`)
+		.then(response => response.json())
+		.then(data => {
+			document.getElementById("currentTemp").innerHTML = `current temp: ${data.main.temp.toFixed(0)}°F`
+			document.getElementById("feelsLike").innerHTML = `feels like: ${data.main.feels_like.toFixed(0)}°F`
+			document.getElementById("imgM").src = "http://openweathermap.org/img/w/" + data.weather.icon
+		})
+
+
+	fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${searchedCity.value}&units=imperial&appid=f419cd27b5d7777610c83ec9680dce19`)
+		.then(response => response.json())
+		.then(data => {
+			for (i = 0; i < 5; i++) {
+				document.getElementById("day" + (i + 1) + "min").innerHTML = "Min: " + Number(data.list[i].main.temp_min).toFixed(0) + "°F";
+			}
+			for (i = 0; i < 5; i++) {
+				document.getElementById("day" + (i + 1) + "max").innerHTML = "Max: " + Number(data.list[i].main.temp_max).toFixed(0) + "°F";
+			}
+			for (i = 0; i < 5; i++) {
+				document.getElementById("img" + (i + 1)).src = " http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png";
+			}
+		})
+
+		.catch(err => ("something went wrong"))
+}
+const d = new Date();
+const weekday = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+
+function checkDay(day) {
+	if (day + d.getDay() > 6) {
+		return day + d.getDay() - 7;
+	}
+	else {
+		return day + d.getDay();
+	}
+}
+
+for (i = 0; i < 5; i++) {
+	document.getElementById("day" + (i + 1)).innerHTML = weekday[checkDay(i)];
+}
+
+
+
+
+
+
+
+
+
+
+
+let APIKEY = "k97KrpZv1g8ZfdGW7o9ZVsQphqFCguwz";
+
+
+document.addEventListener("DOMContentLoaded", init);
+
+
+function init() {
+	document.getElementById("btnSearch").addEventListener("click", ev => {
+		ev.preventDefault(); //to stop the page reload
+		let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=9&q=`;
+		let str = document.getElementById("search").value.trim();
+		url = url.concat(str);
+		console.log(url);
+		fetch(url)
+			.then(response => response.json())
+			.then(content => {
+				for (var i = 0; i < 10; i++) {
+					console.log(content.data);
+					console.log("META", content.meta);
+					document.getElementById("gif" + (i + 1)).src = content.data[i].images.downsized.url;
+					document.getElementById("gif" + (i + 1)).alt = content.data[i].title;
+					document.getElementById("caption" + (i + 1)).innerHTML = content.data[i].title;
+					document.querySelector("#search").value = str;
+				};
+			})
+			.catch(err => {
+				//   console.error(err);
+			});
+	});
+}
+
+
+
+
+const unspsearchbtn = document.getElementById("btnSearchunsp");
+
+unspsearchbtn.addEventListener("click", loadImg);
+function loadImg(e) {
+	e.preventDefault();
+	const searchedunsp = document.getElementById("searchunsp");
+	fetch(`https://api.unsplash.com/search/photos?query=${searchedunsp.value}&per_page=20&client_id=e0lcFxGydphS57T0cGmgW1jizTfMapI4n6W9l5SxISU`)
+		.then(response => {
+			return response.json();
+		})
+		.then(imgdata => {
+
+			for (let i = 0; i < imgdata.results.length; i++) {
+
+				document.getElementById("unspimg" + (i + 1)).src = imgdata.results[i].urls.regular;
+				document.getElementById("unspimgcaption" + (i + 1)).innerHTML = imgdata.results[i].alt_description;
+
+			}
+		})
+}
+
+
+
+
+
+
+
+
+
+const get_meal_btn = document.getElementById('get_meal');
+const meal_container = document.getElementById('meals');
+
+get_meal_btn.addEventListener('click', () => {
+
+	fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+		.then(res => res.json())
+		.then(res => {
+			createMeal(res.meals[0]);
+			console.log(res.meals[0].strMeal);
+
+
+		});
+});
+
+window.onload = () => {
+
+	fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+		.then(res => res.json())
+		.then(res => {
+			createMeal(res.meals[0]);
+			console.log(res.meals[0].strMeal);
+
+
+		})
+}
+
+const createMeal = (meal) => {
+
+	
+	const ingredients = [];
+	// Get all ingredients from the object. Up to 20
+	for (let i = 1; i <= 40; i++) {
+		if (meal[`strIngredient${i}`]) {
+			ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`)
+		} else {
+			// Stop if no more ingredients
+			break;
+		}
+	}
+
+	const newInnerHTML = `
+    <div class="row">
+      <div class="columns five">
+        <img src="${meal.strMealThumb}" alt="Meal Image">
+        ${meal.strCategory ? `<p><strong>Category:</strong> ${meal.strCategory}</p>` : ''}
+        ${meal.strArea ? `<p><strong>Area:</strong> ${meal.strArea}</p>` : ''}
+        ${meal.strTags ? `<p><strong>Tags:</strong> ${meal.strTags.split(',').join(', ')}</p>` : ''}
+        <h5>Ingredients:</h5>
+        <ul>
+          ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
+        </ul>
+      </div>
+      <div class="columns seven">
+        <h4>${meal.strMeal}</h4>
+        <p>${meal.strInstructions}</p>
+      </div>
+    </div>
+    ${meal.strYoutube ? `
+    <div class="row">
+      <h5>Video Recipe</h5>
+      <div class="videoWrapper">
+        <iframe width="420" height="315"
+        src="https://www.youtube.com/embed/${meal.strYoutube.slice(-11)}">
+        </iframe>
+      </div>
+    </div>` : ''}
+  `;
+
+	meal_container.innerHTML = newInnerHTML;
+}
+
+// const createMeal = (meal) => {
+// 	document.querySelector("#foodtitle").innerHTML = meal.strMeal;
+// 	document.querySelector("#foodcategory").innerHTML = "Category: " + meal.strCategory;
+// 	document.querySelector("#foodcountry").innerHTML = "Country: " + meal.strArea;
+// 	document.querySelector("#foodingredients").innerHTML = meal.strIngredient1 + meal.strIngredient2 + meal.strIngredient3
+
+
+// }
